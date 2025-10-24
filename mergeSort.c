26ffbdf117ef6b9b0d84529ca4dfdf51a3024336
cp14 <cp14@lad.pucrs.br>
2025-10-23 23:23:17 -0300

    pre-merge pseudocode

--- mergeSort.c
+++ mergeSort.c
@@ -1,46 +1,75 @@
-MPI_Init();
-my_rank = MPI_Comm_rank();  // pega pega o numero do processo atual (rank)
+#include <stdio.h>
+#include <stdlib.h>
 
-// recebo vetor
+#define DEBUG 1            // comentar esta linha quando for medir tempo
+#define ARRAY_SIZE 40      // trabalho final com o valores 10.000, 100.000, 1.000.000
 
-if ( my_rank != 0 )
-   {
-   MPI_Recv ( vetor, pai);                       // não sou a raiz, tenho pai
-   MPI_Get_count(&Status, MPI_INT, &tam_vetor);  // descubro tamanho da mensagem recebida
-   }
-else
-   {
-   tam_vetor = VETOR_SIZE;               // defino tamanho inicial do vetor
-   Inicializa ( vetor, tam_vetor );      // sou a raiz e portanto gero o vetor - ordem reversa
-   }
+void bs(int n, int * vetor)
+{
+    int c=0, d, troca, trocou =1;
 
-// dividir ou conquistar?
+    while (c < (n-1) & trocou )
+        {
+        trocou = 0;
+        for (d = 0 ; d < n - c - 1; d++)
+            if (vetor[d] > vetor[d+1])
+                {
+                troca      = vetor[d];
+                vetor[d]   = vetor[d+1];
+                vetor[d+1] = troca;
+                trocou = 1;
+                }
+        c++;
+        }
+}
 
-if ( tam_vetor <= delta )
-   BubbleSort (vetor);  // conquisto
-else
-       {
-    // dividir
-    // quebrar em duas partes e mandar para os filhos
+int main()
+{
+    int vetor[ARRAY_SIZE];
+    int i;
 
-    MPI_Send ( &vetor[0], filho esquerda, tam_vetor/2 );  // mando metade inicial do vetor
-    MPI_Send ( &vetor[tam_vetor/2], filho direita , tam_vetor/2 );  // mando metade final
+    for (i=0 ; i<ARRAY_SIZE; i++)              /* init array with worst case for sorting */
+        vetor[i] = ARRAY_SIZE-i;
+   
 
-    // receber dos filhos
+    #ifdef DEBUG
+    printf("\nVetor: ");
+    for (i=0 ; i<ARRAY_SIZE; i++)              /* print unsorted array */
+        printf("[%03d] ", vetor[i]);
+    #endif
 
-    MPI_Recv ( &vetor[0], filho esquerda);            
-    MPI_Recv ( &vetor[tam_vetor/2], filho direita);   
+    bs(ARRAY_SIZE, vetor);                     /* sort array */
 
-    // intercalo vetor inteiro
+    #ifdef DEBUG
+    printf("\nVetor: ");
+    for (i=0 ; i<ARRAY_SIZE; i++)                              /* print sorted array */
+        printf("[%03d] ", vetor[i]);
+    #endif
+
+    return 0;
+}
+
+/* recebe um ponteiro para um vetor que contem as mensagens recebidas dos filhos e            */
+/* intercala estes valores em um terceiro vetor auxiliar. Devolve um ponteiro para este vetor */          */
  
-    Intercala ( vetor );
-    }
+int *interleaving(int vetor[], int tam)
+{
+	int *vetor_auxiliar;
+	int i1, i2, i_aux;
 
-// mando para o pai
+	vetor_auxiliar = (int *)malloc(sizeof(int) * tam);
 
-if ( my_rank !=0 )
-   MPI_Send ( vetor, pai, tam_vetor );  // tenho pai, retorno vetor ordenado pra ele
-else
-   Mostra ( vetor );                    // sou o raiz, mostro vetor
+	i1 = 0;
+	i2 = tam / 2;
 
-MPI_Finalize();
+	for (i_aux = 0; i_aux < tam; i_aux++) {
+		if (((vetor[i1] <= vetor[i2]) && (i1 < (tam / 2)))
+		    || (i2 == tam))
+			vetor_auxiliar[i_aux] = vetor[i1++];
+		else
+			vetor_auxiliar[i_aux] = vetor[i2++];
+	}
+
+	return vetor_auxiliar;
+}
+
